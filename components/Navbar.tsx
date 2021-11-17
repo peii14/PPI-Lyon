1829d0c17385b204a2388df0b831571e31ff06a1
peii14 <gkoma444@gmail.com>
2021-11-17 07:58:05 +0100

    navbar minor fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav className="w-screen bg-black opacity-60 py-5 max-h-16 fixed z-50">
+    <nav className="w-screen bg-primary opacity-80 py-5 max-h-16 fixed z-50">
       <ul className="flex flex-row text-white justify-between max-w-md mx-auto text-center">
         <li>
           <Link href="/">
@@ -32,9 +32,9 @@ const Navbar = () => {
                   leaveFrom="transform scale-100 opacity-100"
                   leaveTo="transform scale-95 opacity-0"
                 >
-                  <Disclosure.Panel className="w-9/12 mx-auto bg-black p-5">
+                  <Disclosure.Panel className="w-9/12 mx-auto bg-primary p-5">
                     <ul className="flex flex-col text-left gap-5">
-                      <li className="hover:bg-opacity-100 ">
+                      <li className="hover:bg-sec ">
                         <Link href="/Keberangkatan">
                           <a>Keberangkatan</a>
                         </Link>
