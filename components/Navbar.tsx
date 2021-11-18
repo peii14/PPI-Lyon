2afcb685b37fe36b18b898f3d6ad86045b2017f0
peii14 <gkoma444@gmail.com>
2021-11-18 18:20:41 +0100

    fixed navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { Disclosure, Transition } from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const router = useRouter();
   return (
-    <nav className="w-screen bg-primary opacity-80 py-5 max-h-16 fixed z-50">
-      <ul className="flex flex-row text-white justify-between max-w-md mx-auto text-center">
-        <li>
-          <Link href="/">
+    <nav className="w-screen bg-primary opacity-80 max-h-16 fixed z-50">
+      <ul className="flex flex-row text-white justify-between max-w-md mx-auto text-center transition-all duration-300">
+        <Link href="/">
+          <li className="transition-all duration-300 hover:bg-white hover:text-primary p-5 cursor-pointer h-full">
             <a>Home</a>
-          </Link>
-        </li>
+          </li>
+        </Link>
         <li className="flex-grow-0">
           <Disclosure>
             {({ open }) => (
               <>
-                <Disclosure.Button className="space-x-3">
+                <Disclosure.Button className="space-x-3 p-5 hover:bg-white hover:text-primary">
                   <span>Guideline</span>
                   <FontAwesomeIcon
                     className={`transform duration-500 
@@ -26,34 +28,34 @@ const Navbar = () => {
                 </Disclosure.Button>
                 <Transition
                   enter="transition duration-300 ease-out"
-                  enterFrom="transform scale-95 opacity-0"
-                  enterTo="transform scale-100 opacity-100"
+                  enterFrom="transform scale-95 translate-10 opacity-0"
+                  enterTo="transform scale-100 translate-0 opacity-100"
                   leave="transition duration-100 ease-out"
-                  leaveFrom="transform scale-100 opacity-100"
-                  leaveTo="transform scale-95 opacity-0"
+                  leaveFrom="transform scale-100 translate-0 opacity-100"
+                  leaveTo="transform scale-95 -translate-20 opacity-0"
                 >
-                  <Disclosure.Panel className="w-9/12 mx-auto bg-primary p-5">
-                    <ul className="flex flex-col text-left gap-5">
-                      <li className="hover:bg-sec ">
-                        <Link href="/Keberangkatan">
+                  <Disclosure.Panel className="w-11/12 mx-auto bg-primary">
+                    <ul className="flex flex-col text-left gap-3">
+                      <Link href="/Keberangkatan">
+                        <li className="transition-all duration-100 px-5 py-1 cursor-pointer hover:bg-white hover:text-primary ">
                           <a>Keberangkatan</a>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href="/TibadiLyon">
+                        </li>
+                      </Link>
+                      <Link href="/TibadiLyon">
+                        <li className="transition-all duration-100 py-1 px-5 cursor-pointer hover:bg-white hover:text-primary">
                           <a>Tiba di Lyon</a>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href="/PerancisJangkaPanjang">
+                        </li>
+                      </Link>
+                      <Link href="/PerancisJangkaPanjang">
+                        <li className="transition-all duration-100 py-1 px-5 cursor-pointer hover:bg-white hover:text-primary">
                           <a>Perancis Jangka Panjang</a>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href="/Budgeting">
+                        </li>
+                      </Link>
+                      <Link href="/Budgeting">
+                        <li className="transition-all duration-100 px-5 pt-1 pb-2 cursor-pointer hover:bg-white hover:text-primary">
                           <a>Budgeting</a>
-                        </Link>
-                      </li>
+                        </li>
+                      </Link>
                     </ul>
                   </Disclosure.Panel>
                 </Transition>
@@ -61,11 +63,11 @@ const Navbar = () => {
             )}
           </Disclosure>
         </li>
-        <li>
-          <Link href="/Gallery">
+        <Link href="/Gallery">
+          <li className="transition-all duration-300 hover:bg-white hover:text-primary p-5 cursor-pointer h-full">
             <a>Gallery</a>
-          </Link>
-        </li>
+          </li>
+        </Link>
       </ul>
     </nav>
   );
