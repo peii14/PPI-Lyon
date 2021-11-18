347bfd7fa5b7c95cb8c8c0a35d3a6b751cad709c
peii14 <gkoma444@gmail.com>
2021-11-18 17:37:56 +0100

    working on keberangkatan page

--- pages/Keberangkatan.tsx
+++ pages/Keberangkatan.tsx
@@ -1,6 +1,12 @@
+import Hero from '../components/Hero'
+import k from '../images/keberangkatan/keberangkatanHero.png'
 const Keberangkatan = ()=>{
     return(
         <>
+        <Hero img={k} />
+        <section className='text-primary p-20'>
+            <h1 className='text-center max-w-xl mx-auto'>Mengajukan visa VLS-TS</h1>
+        </section>
         </>
     )
 }
