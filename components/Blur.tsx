7f8fb038dadd810fcae38e83b5fdced48a09baa4
Gayuh kautaman <gkoma444@gmail.com>
2022-01-17 22:17:58 +0100

    added blur component

--- components/Blur.tsx
+++ components/Blur.tsx
@@ -1,10 +1,25 @@
 import blur1 from "../images/blur1.png";
 import Image from "next/image";
-const Blur = () => {
-  return (
-    <div className="absolute bottom-0 left-3/4 w-3/4">
-      <Image src={blur1} />
-    </div>
-  );
+const Blur = (props) => {
+  if (props.pos == 0) {
+    return (
+      <div className="absolute bottom-0 left-3/4 w-3/4">
+        <Image src={blur1} />
+      </div>
+    );
+  } else if(props.pos == 1) {
+    return (
+      <div className="w-3/4 -left-2/4 bottom-0 absolute align-bottom ">
+        <Image src={blur1} layout="intrinsic" />
+      </div>
+    );
+  }
+  else{
+      return(
+        <div className="w-10/12 absolute">
+            <Image src={blur1} layout="intrinsic" />
+      </div>
+      )
+  }
 };
 export default Blur;
