ecb6b6b38446a737d4ded0c39a454945b074ea3e
Gayuh kautaman <gkoma444@gmail.com>
2022-01-21 18:30:06 +0100

    layout fixed

--- pages/Budgeting.tsx
+++ pages/Budgeting.tsx
@@ -21,121 +21,130 @@ const Budgeting = () => {
           <h2 className="text-center">Biaya Pengeluaran</h2>
           <div className="p-5">
             <Glassmorphism>
-              <div className="grid grid-cols-2 gap-y-5">
+              <div className="grid grid-cols-2 ">
                 <h3 className="text-danger">Pengeluaran Tahunan</h3>
                 <h3>Jumlah</h3>
-                <div>
+                <div className="grid grid-cols-2 col-span-2 my-5">
                   <p>Timbre Fiscal untuk OFII</p>
-                  <p>CVEC</p>
-                  <p>Assurance d’habitation kamu </p>
-                  <p className="font-bold">TOTAL</p>
-                </div>
-                <div>
                   <div className="grid grid-cols-5">
                     <p>60 €</p>
                     <p className="col-span-4">
                       | 85€ untuk titre de séjour di tahun kedua
                     </p>
                   </div>
+                  <p>CVEC</p>
                   <p>90 €</p>
+                  <p>Assurance d’habitation</p>
                   <div className="grid grid-cols-5">
                     <p>50 €</p>
                     <p className="col-span-4">
                       | bisa berkurang berdasarkan pilihan
                     </p>
                   </div>
+                  <p className="font-bold">TOTAL</p>
                   <p className="font-bold">200 €</p>
                 </div>
                 <h3 className="text-danger col-span-2">
                   Pengeluaran Bulanan Essensial
                 </h3>
-                <div className="">
+                <div className="grid grid-cols-2 col-span-2 my-5">
                   <p>Loyer Apartemen</p>
+                  <div className="grid grid-cols-5">
+                    <p>400 €</p>
+                    <p className="col-span-4"> | sebelum dipotong CAF</p>
+                  </div>
                   <p>Charges (Listrik, Gas, Air)</p>
+                  <div className="grid grid-cols-5">
+                    <p>30 € </p>
+                    <p className="col-span-4"> | bisa termasuk Loyer</p>
+                  </div>
                   <p>Abonnement TCL</p>
+                  <div className="grid grid-cols-5">
+                    <p>32 € </p>
+                    <p className="col-span-4">
+                      {" "}
+                      | bisa diganti dengan carnet 10
+                    </p>
+                  </div>
                   <p>Kartu SIM</p>
+                  <div className="grid grid-cols-5">
+                    <p>15 € </p>
+                    <p className="col-span-4"> | bisa dikurangi sampai 10 €</p>
+                  </div>
                   <p>Belanja Bahan Pangan</p>
+                  <div className="grid grid-cols-5">
+                    <p>100 € </p>
+                    <p className="col-span-4">
+                      {" "}
+                      | bisa dikurangi berdasarkan pola makan
+                    </p>
+                  </div>
                   <p>Belanja Kebutuhan (Pribadi)</p>
+                  <div className="grid grid-cols-5">
+                    <p>20 € </p>
+                    <p className="col-span-4">
+                      {" "}
+                      | kebutuhan non pangan (kebersihan, dll)
+                    </p>
+                  </div>
                   <p className="font-bold">Total</p>
-                </div>
-                <div className="grid grid-cols-5">
-                  <p>400 €</p>
-                  <p className="col-span-4"> | sebelum dipotong CAF</p>
-                  <p>30 € </p>
-                  <p className="col-span-4"> | bisa termasuk Loyer</p>
-                  <p>32 € </p>
-                  <p className="col-span-4"> | bisa diganti dengan carnet 10</p>
-                  <p>15 € </p>
-                  <p className="col-span-4"> | bisa dikurangi sampai 10 €</p>
-                  <p>100 € </p>
-                  <p className="col-span-4">
-                    {" "}
-                    | bisa dikurangi berdasarkan pola makan
-                  </p>
-                  <p>20 € </p>
-                  <p className="col-span-4">
-                    {" "}
-                    | kebutuhan non pangan (kebersihan, dll)
-                  </p>
                   <p className="font-bold">597 €</p>
                 </div>
                 <h3 className="text-danger col-span-2">
                   Pengeluaran Bulanan Tambahan Opsional
                 </h3>
-                <div>
+                <div className="grid grid-cols-2 col-span-2 my-5">
                   <p>Mutuelle de Santé</p>
-                  <p>Internet Apartemen</p>
-                  <p>Uang saku belanja</p>
-                  <p>Uang saku restoran, café</p>
-                  <p>Abonnement Aktifitas (Gym, film)</p>
-                  <p className="font-bold">TOTAL</p>
-                </div>
-                <div>
                   <p>15 €</p>
+                  <p>Internet Apartemen</p>
                   <p>20 €</p>
+                  <p>Uang saku belanja</p>
                   <p>100 €</p>
+                  <p>Uang saku restoran, café</p>
                   <p>50 €</p>
+                  <p>Abonnement Aktifitas (Gym, film)</p>
                   <p>20 €</p>
+                  <p className="font-bold">TOTAL</p>
                   <p className="font-bold">205 €</p>
                 </div>
                 <h3 className="text-danger col-span-2">
                   Pengeluaran Sesekali Lainnya
                 </h3>
-                <div>
+                <div className="grid grid-cols-2 col-span-2 my-5">
                   <p>Membeli perlatan dapur</p>
+                  <p>50 €</p>
                   <p>Membeli bantal, selimut, dll</p>
+                  <p>50 €</p>
                   <p>Uang saku liburan</p>
-                  <p className="font-bold">TOTAL</p>
-                </div>
-                <div>
-                  <p>50 €</p>
-                  <p>50 €</p>
                   <div className="grid grid-cols-5">
                     <p>200 €</p>
                     <p className="col-span-4">
                       | liburan di dalam zona Schengen
                     </p>
                   </div>
+                  <p className="font-bold">TOTAL</p>
                   <p className="font-bold">300 €</p>
                 </div>
                 <h3 className="text-danger">Pemasukan Di Prancis</h3>
                 <h3 className="">Harga</h3>
-                <div>
-                  <p>APL dari CAF</p>
-                  <p>Pekerjaan temps partiel lama</p>
-                  <p className="font-bold">Total</p>
+                <div className="grid grid-cols-2 col-span-2 my-5">
                 </div>
+                <p>APL dari CAF</p>
                 <div className="grid grid-cols-5">
                   <p>85 €</p>
                   <p className="col-span-4">
                     | bisa lebih tinggi berdasarkan loyer
                   </p>
+                </div>
+                <p>Pekerjaan temps partiel lama</p>
+                <div className="grid grid-cols-5">
                   <p>750 €</p>
                   <p className="col-span-4">
                     | rata-rata pendapatan temps partiel
                   </p>
-                  <p className="font-bold">835 €</p>
                 </div>
+                <p className="font-bold">Total</p>
+                <p className="font-bold">835 €</p>
               </div>
             </Glassmorphism>
           </div>
